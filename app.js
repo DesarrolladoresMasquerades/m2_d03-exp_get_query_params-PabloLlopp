e3394e44cda04c7a20a61b72d26dc067fa714145
Pablo López <p.lopezmunoz@gmail.com>
2022-02-09 14:57:03 +0100

    query get post

--- app.js
+++ app.js
@@ -6,12 +6,40 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
 app.use(express.static("public"));
 
-// URL params
+/*
+app.get("/search", (req, res)=>{
+  res.render("search-form");
+})
+app.get("/search-results", (req, res)=>{
+  const searchString = req.query.searchString;
+  const searchDate = req.query.searchDate;
+
+  const searchResults = ["beatles", "nirvana"].filter((el) => el.includes(searchString))
+
+  res.render("search-results", {searchString, searchDate, searchResults});
+})
+*/
+
+app
+  .route("/search")
+  .get((req, res) => {
+    res.render("search-form");
+  })
+  .post((req, res) => {
+    const searchString = req.body.searchString;
+    const searchDate = req.body.searchDate;
+    const searchResults = ["beatles", "nirvana"].filter((el) => el.includes(searchString))
+
+    res.render("search-results", { searchString, searchDate, searchResults });
+  });
 
 app.listen(process.env.PORT, () =>
   console.log(`Running on port: ${process.env.PORT}`)
